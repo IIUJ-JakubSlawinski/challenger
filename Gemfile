c4a4eca105397f0adc99bd650e05cb100409c7ed
piotrplaneta <piotr.planeta@gmail.com>
2012-11-05 10:17:39 +0100

    facebook sign in test improvements

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.8'
 gem 'jquery-rails', '2.0.2'
 gem 'bcrypt-ruby', '3.0.1'
-gem 'omniauth-facebook'
+gem 'omniauth-facebook', '1.4.1'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
--- spec/requests/omniauth_signin_spec.rb
+++ spec/requests/omniauth_signin_spec.rb
@@ -11,20 +11,34 @@ describe "Authentication" do
       let(:user) { FactoryGirl.build(:user) }
       before do
           configure_valid_mock_fb_auth(user)
-          click_link "Sign in with FB"
       end
 
-      it { should have_content "Successfully signed in with facebook" }
-      it { should have_selector "h1", text: user.name }
+      it "should create a user" do
+        expect { click_link "Sign in with FB" }.to change(User, :count)
+      end
 
-      describe "sign out and sign in again" do
-        before do
-          click_link "Sign out"
-          click_link "Sign in with FB"
-        end
+      describe "and redirect back to user page" do
+        before { click_link "Sign in with FB" }
 
         it { should have_content "Successfully signed in with facebook" }
         it { should have_selector "h1", text: user.name }
+
+        describe "followed by sign out and sign in again" do
+          before do
+            click_link "Sign out"
+          end
+
+          it "should not create a user" do
+            expect { click_link "Sign in with FB" }.not_to change(User, :count)
+          end
+
+          describe "and redirect back to user page" do
+            before { click_link "Sign in with FB" }
+
+            it { should have_content "Successfully signed in with facebook" }
+            it { should have_selector "h1", text: user.name }
+          end
+        end
       end
     end
 
@@ -32,22 +46,36 @@ describe "Authentication" do
       let(:user) { FactoryGirl.create(:user) }
       before do
         configure_valid_mock_fb_auth(user)
-        click_link "Sign in with FB"
       end
+
+      it "should not create a user" do
+        expect { click_link "Sign in with FB" }.not_to change(User, :count)
+      end
+
+      describe "and redirect back to root page" do
+        before { click_link "Sign in with FB" }
       
-      it { should have_content "Email your facebook has provided was already used to signup" }
-      it { should_not have_selector "h1", text: user.name }
+        it { should have_content "Email your facebook has provided was already used to signup" }
+        it { should_not have_selector "h1", text: user.name }
+      end
     end
 
     describe "sign up with invalid credentials" do
       let(:user) { FactoryGirl.build(:user) }
       before do
           configure_invalid_mock_fb_auth
-          click_link "Sign in with FB"
       end
 
-      it { should have_content "Error" }
-      it { should_not have_selector "h1", text: user.name }
+      it "should not create a user" do
+        expect { click_link "Sign in with FB" }.not_to change(User, :count)
+      end
+
+      describe "and redirect back to root page" do
+        before { click_link "Sign in with FB" }
+      
+        it { should have_content "Error" }
+        it { should_not have_selector "h1", text: user.name }
+      end
     end
   end
 
