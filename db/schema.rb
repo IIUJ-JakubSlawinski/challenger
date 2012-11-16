664bed7b348aa671dfb8de73f7e7383cb1568b59
Lucas Mokrzycki <mokrzu@gmail.com>
2012-11-16 22:32:43 +0100

    deleted broken code

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121104222829) do
-
-  create_table "groups", :force => true do |t|
-    t.string   "name"
-    t.string   "city"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20121103161659) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
