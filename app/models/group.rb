e3559764984d722238beb1336f2727d746c9a020
Lucas Mokrzycki <mokrzu@gmail.com>
2012-11-16 22:33:54 +0100

    deleted broken code - part 2

--- app/models/group.rb
+++
@@ -1,4 +0,0 @@
-class Group < ActiveRecord::Base
-  attr_accessible :city, :name
-  has_many :users
-end
--- db/migrate/20121104224346_groups_update.rb
+++
@@ -1,7 +0,0 @@
-class GroupsUpdate < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
