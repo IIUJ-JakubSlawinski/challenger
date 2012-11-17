74bd90a7570b04b619525ec7628d3e334eb1a4f7
Lucas Mokrzycki <mokrzu@gmail.com>
2012-11-18 00:29:02 +0100

    fixed associations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ActiveRecord::Base
-  attr_accessible :city, :closed, :name
+    has_many :users
+    attr_accessible :city, :closed, :name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121103161659) do
+ActiveRecord::Schema.define(:version => 20121117190648) do
+
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.string   "city"
+    t.boolean  "closed"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -21,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20121103161659) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.string   "facebook_uid"
+    t.integer  "group_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
