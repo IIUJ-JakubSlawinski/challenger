d29d8ea8e9a8382090144b55afe69ab1bd327eff
Lucas Mokrzycki <mokrzu@gmail.com>
2012-11-21 22:53:31 +0100

    added validation and first tests for group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,10 @@
 class Group < ActiveRecord::Base
-    has_many :users
     attr_accessible :city, :closed, :name
+    
+    has_many :users
+
+    validates :name, presence: true, length: { maximum: 60 }
+    validates :closed, presence: true
+    validates :city, presence: true, length: { minimum: 3, maximum: 50 }
+
 end
