6fa798f5dbfe4b4e26eed361450f07679286a2cb
Lucas Mokrzycki <mokrzu@gmail.com>
2012-11-25 18:48:19 +0100

    added tests for sport model

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,3 +1,5 @@
 class Sport < ActiveRecord::Base
   attr_accessible :name
+
+  validates :name, presence: true, length: { minimum: 2 }  
 end
--- spec/models/sport_spec.rb
+++ spec/models/sport_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Sport do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before { @sport = Sport.new(name: "Darts") }
+
+  subject { @sport }
+
+  it { should respond_to(:name) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do 
+  	before { @sport.name = " " }
+  	it { should_not be_valid }
+  end
+
+  describe "when sport name is too short" do
+    before { @sport.name = "x" }
+    it { should_not be_valid }
+  end
 end
