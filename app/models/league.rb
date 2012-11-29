b6df1c9c2d0b2a94ef817a8d6581109b45a50557
IIUJ-JakubSlawinski <kanngaxx@gmail.com>
2012-11-29 02:19:29 +0100

    Minor fixes to models

--- app/models/league.rb
+++ app/models/league.rb
@@ -5,7 +5,7 @@ class League < ActiveRecord::Base
   has_many :roles
   has_many :users, through: :roles
 
-  attr_accessible :name
+  attr_accessible :name, :place, :place_id
 
   validates :name, presence: true, length: {maximum: 60, minimum: 2}, uniqueness: { case_sensitive: false }
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,7 +1,7 @@
 class Meeting < ActiveRecord::Base
   belongs_to :place
   belongs_to :league
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :place, :place_id
 
   validates :name, presence: true, length: { minimum: 3}
 end
